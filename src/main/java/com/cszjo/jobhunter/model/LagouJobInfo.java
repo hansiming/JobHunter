e8912ab1fd521364084f8fa35b318da7bc550abf
hansiming <csdhsm@163.com>
2017-04-12 19:03:36 +0800

    update

---
+++ src/main/java/com/cszjo/jobhunter/model/LagouJobInfo.java
@@ -0,0 +1,82 @@
+package com.cszjo.jobhunter.model;
+
+/**
+ * Created by Han on 2017/4/11.
+ */
+public class LagouJobInfo {
+
+    //职位名
+    private String positionName;
+    private String workYear;
+    private String education;
+    private String city;
+    //薪资
+    private String salary;
+    private String district;
+    private String companyFullName;
+    private String positionId;
+
+    public String getPositionName() {
+        return positionName;
+    }
+
+    public void setPositionName(String positionName) {
+        this.positionName = positionName;
+    }
+
+    public String getWorkYear() {
+        return workYear;
+    }
+
+    public void setWorkYear(String workYear) {
+        this.workYear = workYear;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getSalary() {
+        return salary;
+    }
+
+    public void setSalary(String salary) {
+        this.salary = salary;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getCompanyFullName() {
+        return companyFullName;
+    }
+
+    public void setCompanyFullName(String companyFullName) {
+        this.companyFullName = companyFullName;
+    }
+
+    public String getPositionId() {
+        return positionId;
+    }
+
+    public void setPositionId(String positionId) {
+        this.positionId = positionId;
+    }
+}
